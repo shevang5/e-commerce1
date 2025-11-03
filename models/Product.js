494744779c8365ad6cf2e9fce1aa980ac0158c46
shevang5 <shevangpalgam@gmail.com>
2025-11-03 08:54:17 +0530

    category error

--- models/Product.js
+++ models/Product.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: String,
-  category: { type: String },
   price: { type: Number, required: true },
   image: String,
   stock: { type: Number, default: 0 }
