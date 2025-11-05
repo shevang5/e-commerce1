dc47b6d970328821b360cb1152931b2e9965c8c7
shevang5 <shevangpalgam@gmail.com>
2025-11-05 18:18:28 +0530

    add to cart added

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,26 +1,34 @@
 import Cart from "../models/Cart.js";
 
 export const addToCart = async (req, res) => {
-  const { productId, quantity } = req.body;
-  let cart = await Cart.findOne({ user: req.user._id });
+  try {
+    const { productId, quantity = 1 } = req.body;
+    let cart = await Cart.findOne({ user: req.user._id });
 
-  if (!cart) {
-    cart = await Cart.create({
-      user: req.user._id,
-      items: [{ product: productId, quantity }]
-    });
-  } else {
-    const itemIndex = cart.items.findIndex(i => i.product.toString() === productId);
-    if (itemIndex > -1) {
-      cart.items[itemIndex].quantity += quantity;
+    if (!cart) {
+      cart = await Cart.create({
+        user: req.user._id,
+        items: [{ product: productId, quantity }],
+      });
     } else {
-      cart.items.push({ product: productId, quantity });
+      const itemIndex = cart.items.findIndex(
+        (i) => i.product.toString() === productId
+      );
+      if (itemIndex > -1) {
+        cart.items[itemIndex].quantity += quantity;
+      } else {
+        cart.items.push({ product: productId, quantity });
+      }
+      await cart.save();
     }
-    await cart.save();
-  }
 
-  res.status(200).json(cart);
+    res.status(200).json(cart);
+  } catch (err) {
+    console.error("Add to cart error:", err);
+    res.status(500).json({ message: err.message });
+  }
 };
+
 
 export const getCart = async (req, res) => {
   const cart = await Cart.findOne({ user: req.user._id }).populate("items.product");
